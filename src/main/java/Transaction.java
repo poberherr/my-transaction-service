ae8615fafda705fa28b1d5813baee1f2c03a343a
Patrick Oberherr <patrick.oberherr@gmail.com>
2016-01-14 09:29:17 +0100

    Added transaction
    I'd still refactor it at a later stage since this is a weird mix out of lombok (bad docu but nice), and manual code - Intellij IDE support is not that good

---
+++ src/main/java/Transaction.java
@@ -0,0 +1,56 @@
+import lombok.Data;
+
+/**
+ * Created by paTimu on 1/13/2016.
+ */
+@Data
+public class Transaction {
+    private long id;
+    private double amount;
+    private String type;
+    private long parent_id;
+
+    public Transaction(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public long getParent_id() {
+        return parent_id;
+    }
+
+    public void setParent_id(long parent_id) {
+        this.parent_id = parent_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", type='" + type + '\'' +
+                ", parent_id=" + parent_id +
+                '}';
+    }
+}
