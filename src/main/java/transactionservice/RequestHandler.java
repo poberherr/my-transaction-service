f8ce8ee00e60976ebe1063ec077a58fd11209bd4
Patrick Oberherr <patrick.oberherr@gmail.com>
2016-01-15 01:48:02 +0100

    Abstraction classes to Handle Requests, Validation & Answers

---
+++ src/main/java/transactionservice/RequestHandler.java
@@ -0,0 +1,7 @@
+package transactionservice;
+
+import java.util.Map;
+
+public interface RequestHandler<V extends Validable> {
+    Answer process(V value, Map<String, String> urlParams, boolean shouldReturnHtml);
+}
---
+++ src/main/java/transactionservice/Validable.java
@@ -0,0 +1,5 @@
+package transactionservice;
+
+public interface Validable {
+    boolean isValid();
+}
